2f215d5a35c29609065bce46265244ec045094ea
Jesse Ogburn <jogburn88@gmail.com>
2022-07-17 18:36:26 -0400

    Cleaned the test file

--- test_Human.py
+++ test_Human.py
@@ -13,23 +13,25 @@ class Testing(ut.TestCase):
 
 class TestHuman(ut.TestCase):
     human = Human("John", "Male")
-    test_name = "john"
+    test_name = "John"
     test_sex = "Male"
     test_get_is_married = False
 
     def test_Human_print_string(self):
         print("Begin testing the Human class \n")
         print("Test if printing a human instance returns the name of the human")
-        #self.assertEqual(self.human.__str__(), self.test_name)
+        self.assertEqual(self.human.__str__(), self.test_name)
 
         try:
             self.assertEqual(self.human.get_first_name(), self.test_name)
-        except AssertionError:
+        except AssertionError as e:
             print("\n get_first_name did not pass the test.")
             print(e)
 
         self.assertEqual(self.human.get_sex(), self.test_sex)
         self.assertEqual(self.human.get_is_married(), self.test_get_is_married, "is_married did not pass")
+
+        print("\nEnd testing the Human class")
 
 if __name__ == '__main__':
     # begin the unittest.main()
